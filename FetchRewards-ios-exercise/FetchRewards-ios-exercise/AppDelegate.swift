f5e02e9915b4283ddcee31480ddb3f7214c638a5
iyinraphael <iyinraphael@gmail.com>
2021-02-02 14:28:56 -0800

    initial set up

---
+++ FetchRewards-ios-exercise/FetchRewards-ios-exercise/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  FetchRewards-ios-exercise
+//
+//  Created by Iyin Raphael on 2/2/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = SeatGeekViewController()
+        window?.rootViewController = UINavigationController(rootViewController: vc)
+        window?.makeKeyAndVisible() 
+        
+        return true
+    }
+
+}
+
---
+++ FetchRewards-ios-exercise/FetchRewards-ios-exercise/View Controller/SeatGeekViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SeatGeekViewController.swift
+//  FetchRewards-ios-exercise
+//
+//  Created by Iyin Raphael on 2/2/21.
+//
+
+import UIKit
+
+class SeatGeekViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
