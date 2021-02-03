84171fc66e61286a8c4d51de4f9be4c617000e7d
iyinraphael <iyinraphael@gmail.com>
2021-02-02 18:35:12 -0800

    uitableview set up

--- FetchRewards-ios-exercise/FetchRewards-ios-exercise/View Controller/SeatGeekEventViewController.swift
+++ FetchRewards-ios-exercise/FetchRewards-ios-exercise/View Controller/SeatGeekEventViewController.swift
@@ -9,8 +9,32 @@ import UIKit
 
 class SeatGeekViewController: UIViewController {
 
+    //MARK: - Properties
+    let reuseIdentifier = "eventCell"
+    var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        tableView = UITableView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height))
+        tableView.register(GeekSeatEventTableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.dataSource = self
     }
+}
+
+extension SeatGeekViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as? GeekSeatEventTableViewCell
+        else { return UITableViewCell() }
+        
+        return cell
+    }
+    
+    
 }
